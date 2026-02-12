81ad72e3bc60ea253ea2f7a8536fcf351061b66d
Romeo558 <rkoshelev2001@gmail.com>
2026-02-12 07:57:12 +0300

    Added sound to a door in the start room, connected start and puzzle rooms with a loading screen.

--- Scripts/start_door_script.gd
+++ Scripts/start_door_script.gd
@@ -13,6 +13,14 @@ func update_interaction_text():
 func perform_interaction():
 	if !opened:
 		animation_player.play("start_door_open")
+		var sound = AudioStreamPlayer2D.new()
+		sound.stream = preload("res://Assets/Audio/door_open.wav")
+		sound.volume_db = 2
+		add_child(sound)
+		sound.play()
+		await sound.finished
+		sound.queue_free()
 		opened = true
+		interaction_text = "Дверь заклинило в этом положении, закрыть обратно нельзя."
 	else:
 		interaction_text = "Дверь заклинило в этом положении, закрыть обратно нельзя."
--- Scripts/trigger_stroom_left.gd
+++ Scripts/trigger_stroom_left.gd
@@ -3,4 +3,10 @@ extends BaseTrigger
 @export var monologue_id: String = "intro_01"
 
 func on_trigger_enter(body):
-	print("test successful")
+	MonologueSystem.play_monologue("T_good_job")
+	await MonologueSystem.monologue_finished
+	await get_tree().create_timer(1).timeout
+	LoadingScreen.start(3, "А ты любишь головоломки?")
+	await get_tree().create_timer(3).timeout
+	LoadingScreen.allow_fade_out()
+	get_tree().change_scene_to_file("res://Scenes/puzzle_room.tscn")
